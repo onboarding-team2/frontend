5e415e5fa0c2de8af44860f360df0f10cb7b36a3
juuurr <juuurr@naver.com>
2026-06-13 06:34:52 +0900

    update: ALB라우팅 백엔드 공통 prefix 추가

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8080'
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8080/api'
 
 export const DEMO_COMPANY_ID = 'C005'
 
